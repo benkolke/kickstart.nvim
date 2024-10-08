e67d9533378bba9783e4b99be91ea06757c29b5b
benjamin.kolke <benjamin.kolke@workday.com>
2024-10-08 09:48:42 -0700

    fixed folder functionality

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -25,12 +25,22 @@ return {
         mappings = {
           ['\\'] = 'close_window',
           ['<tab>'] = function(state)
-            state.commands['open'](state)
-            vim.cmd 'Neotree reveal'
+            local node = state.tree:get_node()
+            if require('neo-tree.utils').is_expandable(node) then
+              state.commands['toggle_node'](state)
+            else
+              state.commands['open'](state)
+              vim.cmd 'Neotree reveal'
+            end
           end,
           ['<cr>'] = function(state)
-            state.commands['open'](state)
-            state.commands['close_window'](state)
+            local node = state.tree:get_node()
+            if require('neo-tree.utils').is_expandable(node) then
+              state.commands['toggle_node'](state)
+            else
+              state.commands['open'](state)
+              state.commands['close_window'](state)
+            end
           end,
         },
       },
