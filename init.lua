171dcd7eeeda12ed7bc13b0f0ab031b1008e604a
Ben Kolke <benjamin.kolke@workday.com>
2026-01-21 10:31:23 -0800

    better searching

--- init.lua
+++ init.lua
@@ -502,10 +502,44 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files { hidden = true }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('v', '<leader>sw', function()
+        -- Visual mode: get the selected text
+        vim.cmd 'noau normal! "vy"'
+        local search_term = vim.fn.trim(vim.fn.getreg 'v')
+        vim.fn.setreg('v', '') -- Clear the register
+
+        if search_term and search_term ~= '' then
+          builtin.grep_string {
+            search = search_term,
+            additional_args = { '--hidden' },
+          }
+        end
+      end, { desc = '[S]earch current [W]ord' })
+
+      vim.keymap.set('n', '<leader>sw', function()
+        -- Normal mode: use word under cursor
+        local search_term = vim.fn.expand '<cword>'
+        if search_term and search_term ~= '' then
+          builtin.grep_string {
+            search = search_term,
+            additional_args = { '--hidden' },
+          }
+        end
+      end, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep {
+          additional_args = { '--hidden' },
+        }
+      end, { desc = '[S]earch by [G]rep (include gitignore)' })
+      vim.keymap.set('n', '<leader>sG', function()
+        builtin.live_grep {
+          additional_args = { '--hidden', '--no-ignore' },
+        }
+      end, { desc = '[S]earch by [G]rep (include gitignore)' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
